a254567566906eaa809198fabafc5d25c3dfe29c
Troter2 <47522460+Troter2@users.noreply.github.com>
2021-04-06 15:47:51 +0200

    test backup

--- application/controllers/Public_controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Public_controller extends CI_Controller
-{
-    public function __construct()
-    {
-        parent:: __construct();
-    }
-}
--- application/views/pages/home.php
+++
@@ -1,2 +0,0 @@
-<h2>Pagina super home hello wolrd</h2>
-<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod incidunt facilis iusto quis eum unde nostrum iste nemo voluptate ducimus perferendis saepe ea atque minus praesentium laborum, tempora repudiandae dolore?</p>
