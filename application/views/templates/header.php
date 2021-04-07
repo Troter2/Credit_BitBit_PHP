cb4e108cf6e5b9a37cc0953adad6f61699cb8b02
Troter2 <47522460+Troter2@users.noreply.github.com>
2021-04-07 19:57:35 +0200

    modifica header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,7 @@
     <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/mycss.css'); ?>">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-    
+
 
     <title>CodeIgniter Tutorial</title>
 </head>
@@ -11,15 +11,20 @@
     </head>
 
     <body id="page-top">
-    <script src="<?=base_url('assets/js/bootstrap.js')?>"></script>
+        <script src="<?= base_url('assets/js/bootstrap.js') ?>"></script>
         <!-- Navigation -->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
             <div class="container">
-                <a class="nav-link" href="<?php echo base_url();?>"><i class="fa fa-home"></i>Home</a>
-                
-                
+                <a class="nav-link" href="<?php echo base_url(); ?>"><i class="fa fa-home"></i>Home</a>
+
+
             </div>
         </nav>
+        <div class="container" style="padding-top: .5rem; padding-bottom: .5rem;">
+            <a class="nav-link" href="<?php echo base_url(); ?>"><i class="fa fa-home"></i>Home</a>
+
+
+        </div>
         <?php
         //   foreach($fondos as $colors){
         //   echo "<h1>". $colors . "</h1>"; }
