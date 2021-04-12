3863df36497fd8071fa67dd0214a2f19d9529058
Troter2 <sergivilamonguia@gmail.com>
2021-04-12 14:36:30 +0200

    sidebar
    Afegim funcionalitat sidebar preparada per acceptar cualsevol altre codi dins, necessita modificacio per a que no es mogui

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,7 @@
+
+                </div>
+            </div>
+        </div>
 <footer class="text-center text-white fixed-bottom" style="background-color: #21081a;">
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,22 +10,38 @@
 <body>
     </head>
 
-    <body id="page-top">
+    <body id="page-top" data-spy="scroll" data-target=".navbar">
         <script src="<?= base_url('assets/js/bootstrap.js') ?>"></script>
         <!-- Navigation -->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
             <div class="container">
                 <a class="nav-link" href="<?php echo base_url(); ?>"><i class="fa fa-home"></i>Home</a>
-
-
             </div>
         </nav>
         <div class="container" style="padding-top: .5rem; padding-bottom: .5rem;">
             <a class="nav-link" href="<?php echo base_url(); ?>"><i class="fa fa-home"></i>Home</a>
-
-
         </div>
         <?php
         //   foreach($fondos as $colors){
         //   echo "<h1>". $colors . "</h1>"; }
         ?>
+        <div class="container-fluid">
+            <div class="row sidebar">
+                <div class="col col-sm-1 bg-success">
+                    <div class=" align-middle my-2 justify-content-center menu-item">
+                        <i class="fa fa-camera " style="font-size: 30px;"></i>
+                    </div>
+                    <div class=" align-middle my-2 justify-content-center">
+                        <i class="fa fa-camera " style="font-size: 30px;"></i>
+                    </div>
+                    <div class=" align-middle my-2 justify-content-center">
+                        <i class="fa fa-camera " style="font-size: 30px;"></i>
+                    </div>
+                    <div class=" align-middle my-2 justify-content-center">
+                        <i class="fa fa-camera " style="font-size: 30px;"></i>
+                    </div>
+                </div>
+                <div class="col col-sm-11 bg-secondary">
+                    a<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                    a<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                    a<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
