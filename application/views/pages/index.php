22c21a3a911b6fedf7e6862344b9a6bac17e88f3
JoeLopezD <72604904+JoeLopezD@users.noreply.github.com>
2021-04-14 00:23:27 +0200

    HomePage con alert messages e imagenes proyectos

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,41 +1,122 @@
-<div style="min-height: auto;">test Panaderia cuando serás mia
+<div style="min-height: auto;">
 
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://image.shutterstock.com/image-vector/sample-stamp-grunge-texture-vector-260nw-1389188336.jpg" class="d-block w-100" alt="...">
+  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
-    <div class="carousel-item">
-      <img src="https://fujifilm-x.com/wp-content/uploads/2019/08/x-t30_sample-images02.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://miro.medium.com/max/1838/1*MI686k5sDQrISBM6L8pf5A.jpeg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-<br><br><br>
-
-
-<div class="col-sm-3">
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="carousel-inner">
+      <div class="carousel-item active ">
+        <img src="https://fujifilm-x.com/wp-content/uploads/2019/08/x-t3_sample-images02.jpg" class="d-block w-100 h-25" alt="Sample image">
+      </div>
+      <div class="carousel-item">
+        <img src="https://fujifilm-x.com/wp-content/uploads/2019/08/x-t30_sample-images02.jpg" class="d-block w-100 h-25" alt="Sample image">
+      </div>
+      <div class="carousel-item">
+        <img src="https://www.phe.es/wp-content/uploads/2017/03/mt-sample-background-e1496164073121.jpg" class="d-block w-100 h-25" alt="Sample image">
       </div>
     </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <br><br><br>
+
+
+
+
+
+
+
+
+
+  <div class="row  g-4">
+    <div class="col col-sm-9 row">
+      <div class="col col-sm-4">
+        <div class="card">
+          <img src="https://mdbootstrap.com/img/new/standard/city/041.jpg" class="card-img-top" alt="Sample image" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col col-sm-4">
+        <div class="card">
+          <img src="https://mdbootstrap.com/img/new/standard/city/042.jpg" class="card-img-top" alt="Sample image" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col col-sm-4">
+        <div class="card">
+          <img src="https://mdbootstrap.com/img/new/standard/city/043.jpg" class="card-img-top" alt="Sample image" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural lead-in to
+              additional content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col col-sm-4">
+        <div class="card">
+          <img src="https://mdbootstrap.com/img/new/standard/city/041.jpg" class="card-img-top" alt="Sample image" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col col-sm-4">
+        <div class="card">
+          <img src="https://mdbootstrap.com/img/new/standard/city/042.jpg" class="card-img-top" alt="Sample image" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col col-sm-4">
+        <div class="card">
+          <img src="https://mdbootstrap.com/img/new/standard/city/043.jpg" class="card-img-top" alt="Sample image" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural lead-in to
+              additional content.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col col-sm-3 card" >
+    <div class="d-grid gap-3">
+  <div class="p-2 bg-light border">New alert</div>
+  <div class="p-2 bg-light border">New alert</div>
+  <div class="p-2 bg-light border">New alert</div>
 </div>
 
-
+    </div>
+  </div>
