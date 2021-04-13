50a97ff5d051e96ce18542ae849a44deff51a0ad
Troter2 <sergivilamonguia@gmail.com>
2021-04-13 15:27:22 +0200

    NEW SIDEBAR

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,19 +1,18 @@
 </div>
 </div>
 </div>
-<div class="text-center text-white">
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        <em> © 2020 Copyright</em>
-    </div>
-</div>
-<footer class="text-center text-white fixed-bottom" style="background-color: #21081a;">
+
+<footer class="text-center text-white " style="background-color: #21081a;">
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         <em> © 2020 Copyright</em>
     </div>
     <!-- Copyright -->
 </footer>
-
+<script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/popper.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
 </body>
 
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,10 @@
 <head>
     <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/mycss.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
     <title>CodeIgniter Tutorial</title>
@@ -13,53 +16,65 @@
     <body id="page-top" data-spy="scroll" data-target=".navbar">
         <script src="<?= base_url('assets/js/bootstrap.js') ?>"></script>
         <!-- Navigation -->
-        <div class="fixed-top">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark " id="mainNav">
-            <div class="container">
-                <a class="nav-link" href="<?php echo base_url(); ?>"><i class="fa fa-home"></i>Home</a>
-            </div>
+            <div class="wrapper d-flex align-items-stretch">
+                <nav id="sidebar" class="active">
+                    <h1><a href="index.html" class="logo">M.</a></h1>
+                    <ul class="list-unstyled components mb-5">
+                        <li class="active">
+                            <a href="#"><span class="fa fa-home"></span> Home</a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="fa fa-user"></span> About</a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="fa fa-sticky-note"></span> Blog</a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="fa fa-cogs"></span> Services</a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="fa fa-paper-plane"></span> Contacts</a>
+                        </li>
+                    </ul>
 
-            <div class="container ; justify-content-end">
-                <a class="nav-link" href="<?php echo base_url(); ?>"><i class="fa fa-user"></i>Profile</a>
-            </div>
-        </nav>
-        <div class="container-fluid ">
-            <div class="row">
-                <div class=" col col-sm-1 bg-success bg-success justify-content-center ">
-                        <div class=" row align-middle py-2 justify-content-center menu-item">
-                            <i class="fa fa-camera " style="font-size: 30px;"></i>
+                    <div class="footer">
+                        <p>
+                            Copyright &copy;<script>
+                                document.write(new Date().getFullYear());
+                            </script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+                        </p>
+                    </div>
+                </nav>
+
+                <!-- Page Content  -->
+                <div id="content" class="pt-4 pb-5 px-4 ">
+
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <div class="container-fluid">
+
+                            <button type="button" id="sidebarCollapse" class="btn btn-primary">
+                                <i class="fa fa-bars"></i>
+                                <span class="sr-only">Toggle Menu</span>
+                            </button>
+                            <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <i class="fa fa-bars"></i>
+                            </button>
+
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="nav navbar-nav ml-auto">
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="#">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">About</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Portfolio</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <div class=" row align-middle py-2 justify-content-center menu-item">
-                            <i class="fa fa-camera " style="font-size: 30px;"></i>
-                        </div><div class=" row align-middle py-2 justify-content-center menu-item">
-                            <i class="fa fa-camera " style="font-size: 30px;"></i>
-                        </div><div class=" row align-middle py-2 justify-content-center menu-item">
-                            <i class="fa fa-camera " style="font-size: 30px;"></i>
-                        </div>
-                </div>
-            </div>
-        </div>
-        </div>
-        
-        <div class="container" style="padding-top: .5rem; padding-bottom: .5rem;">
-            <a class="nav-link" href="<?php echo base_url(); ?>"><i class="fa fa-home"></i>Home</a>
-        </div>
-        <?php
-        //   foreach($fondos as $colors){
-        //   echo "<h1>". $colors . "</h1>"; }
-        ?>
-        <div class="container-fluid ">
-            <div class="row">
-                <div class=" col col-sm-1 bg-success bg-success justify-content-center ">
-                        <div class=" row align-middle py-2 justify-content-center menu-item">
-                            <i class="fa fa-camera " style="font-size: 30px;"></i>
-                        </div>
-                        <div class=" row align-middle py-2 justify-content-center menu-item">
-                            <i class="fa fa-camera " style="font-size: 30px;"></i>
-                        </div><div class=" row align-middle py-2 justify-content-center menu-item">
-                            <i class="fa fa-camera " style="font-size: 30px;"></i>
-                        </div><div class=" row align-middle py-2 justify-content-center menu-item">
-                            <i class="fa fa-camera " style="font-size: 30px;"></i>
-                        </div>
-                </div>
-                <div class="col col-sm-11 bg-secondary">
+                    </nav>
