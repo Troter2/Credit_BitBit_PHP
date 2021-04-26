d6d85edf2d08e439eeb0779c47259ac86d9a571a
Troter2 <sergivilamonguia@gmail.com>
2021-04-26 14:45:45 +0200

    update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -19,7 +19,7 @@
         <!-- Navigation -->
         <div class="wrapper d-flex align-items-stretch">
             <nav id="sidebar" class="active">
-                <h1><a href="index.html" class="logo">M.</a></h1>
+                <h1><a href="<?php echo base_url('/home') ?>" class="logo">M.</a></h1>
                 <ul class="list-unstyled components mb-5">
                     <li class="active">
                         <a href="<?php echo base_url('/home') ?>"><span class="fa fa-home"></span> Home</a>
@@ -41,11 +41,11 @@
                             <div class="dropdown-menu">
                                 <!-- Dropdown menu links -->
                                 <ul class="pl-1">
-                                    <li class="dropdown-item"><a class="text-dark" href="#">Usuaris</a></li>
-                                    <li class="dropdown-item"><a class="text-dark" href="#">Correu</a></li>
-                                    <li class="dropdown-item"><a class="text-dark" href="#">Modificar Home</a></li>
-                                    <li class="dropdown-item"><a class="text-dark" href="#">MODIFICAR OPCIONS CONTACTE</a></li>
-                                    <li class="dropdown-item"><a class="text-dark" href="#">dasboard</a></li>
+                                    <li class="dropdown-item"><a class="text-dark" href="<?php echo base_url('/contact') ?>">Gestor Usuaris</a></li>
+                                    <li class="dropdown-item"><a class="text-dark" href="#">Gestor </a></li>
+                                    <li class="dropdown-item"><a class="text-dark" href="#">Gestor </a></li>
+                                    <li class="dropdown-item"><a class="text-dark" href="#">Gestor </a></li>
+                                    <li class="dropdown-item"><a class="text-dark" href="#">Gestor </a></li>
                                 </ul>
                             </div>
                         </div>
