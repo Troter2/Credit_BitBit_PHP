6a8cb3055d8e41bc773d2581f6b2b471e24bb72b
Troter2 <47522460+Troter2@users.noreply.github.com>
2021-05-18 19:23:38 +0200

    save image correcte

--- application/controllers/Private/Tecnic_controller.php
+++ application/controllers/Private/Tecnic_controller.php
@@ -4,6 +4,7 @@ class Tecnic_controller extends Private_controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->library('upload');
     }
 
     public function update_inci_page()
@@ -11,6 +12,7 @@ class Tecnic_controller extends Private_controller
         $this->load->library('ion_auth');
         $this->load->library('form_validation');
         $this->load->model('inci_model');
+
 
         $data['status'] = $this->inci_model->get_status();
         $data['material'] = $this->inci_model->get_material();
@@ -32,37 +34,69 @@ class Tecnic_controller extends Private_controller
         $this->load->library('ion_auth');
         $this->load->helper('url');
         $this->load->helper('form');
-       
-        $config['upload_path']          = './uploads/';
+        $path="assets/uploads/incidencies/".$_SESSION['inci']['id_inci'];
+
+        if (!is_dir ($path)) 
+            mkdir($path);
+        // mkdir(realpath($path));
+
+        $config['upload_path']          = $path;
         $config['allowed_types']        = 'gif|jpg|png';
-        $config['max_size']             = 100;
-        $config['max_width']            = 1024;
-        $config['max_height']           = 768;
         $config['encrypt_name']         = true;
 
-        $this->load->library('upload', $config);
+        $this->upload->initialize($config);
 
-        if (!$this->upload->do_upload('doc1')) {
-            $error = array('error' => $this->upload->display_errors());
-        } else {
-            $data = array('upload_data' => $this->upload->data());
-            $data = array(
-                'image' => $data['upload_data']['file_name'],
-                'id_tasca' => $_SESSION['tasca']['id_tasca']
-            );
+        for ($i=1;$i<4;$i++)
+        {
+            if (isset($_FILES["doc".$i]) && $_FILES["doc".$i]["name"]!="")
+            {
+                if (!$this->upload->do_upload('doc'.$i)) {
+                    $error = array('error' => $this->upload->display_errors());
+                    echo "ERROR: " . $i . "<br>";
+                    print_r($error);
+                    die;
+                } else {
+                    $data = array('upload_data' => $this->upload->data());
+                    $data = array(
+                        'image' => $data['upload_data']['file_name'],
+                        'path' => "assets/test/uploads/incidencies/".$_SESSION['inci']['id_inci'],
+                        'id_tasca' => $_SESSION['tasca']['id_tasca']
+                    );
+                    $this->db->insert('documents_tasques', $data);
+                    print_r($data);
+                    die;
+                    
+                }
+            }
 
-            $this->db->insert('documents_tasques', $data);
         }
+
+
+        
+
+
+
+/*
+
         if (!$this->upload->do_upload('doc2')) {
             $error = array('error' => $this->upload->display_errors());
+            echo "2222222222222";
+            print_r($error);
+            die;
         } else {
             $data = array('upload_data' => $this->upload->data());
         }
+        
         if (!$this->upload->do_upload('doc3')) {
             $error = array('error' => $this->upload->display_errors());
+            echo "3333333333333";
+            print_r($error);
+            die;
         } else {
             $data = array('upload_data' => $this->upload->data());
         }
+*/
+
         $idTasca = $_SESSION['tasca']['id_tasca'];
         $estatus = $this->input->post('estatus');
         $this->db->set('id_estat', $estatus);
@@ -79,7 +113,7 @@ class Tecnic_controller extends Private_controller
         $this->db->set('canvas', $canvas);
         $this->db->where('id_tasca', $idTasca);
         $this->db->update('tasques');
-
+die('finito');
         redirect(base_url('tecnic/tasques/success/'));
     }
 }
